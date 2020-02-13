e3981d19ce8c0c7ed134454705849ae75b5884c0
xlcod <alvaro.pernas@live.u-tad.com>
2020-02-13 19:39:42 +0100

    detail test added

--- test/remote_repository_test.dart
+++ test/remote_repository_test.dart
@@ -61,5 +61,101 @@ void main() {
       expect(movies[0].poster_path, "/7IiTTgloJzvGI1TAYymCfbfl3vT.jpg");
       expect(movies[0].id, 496243);
     });
+    test('Should return MOVIESDETAIL when request ', () async {
+      when(_client.get(
+              'http://api.themoviedb.org/3/movie/41342?api_key=77335f53286ea3ce074ab21558a8fd05'))
+          .thenAnswer((_) async {
+        return Response(
+            '''
+      {
+    "adult": false,
+    "backdrop_path": "/zNYbiNjYaeK8EDGFjXUJL7E54Bv.jpg",
+    "belongs_to_collection": null,
+    "budget": 0,
+    "genres": [
+        {
+            "id": 28,
+            "name": "Action"
+        },
+        {
+            "id": 80,
+            "name": "Crime"
+        },
+        {
+            "id": 18,
+            "name": "Drama"
+        }
+    ],
+    "homepage": "http://www.baselinethemovie.com",
+    "id": 41342,
+    "imdb_id": "tt1390397",
+    "original_language": "en",
+    "original_title": "Baseline",
+    "overview": "The Baseline is the centre of the East London grime club scene and the queue regularly stretches around the block. Danny works on the door - breaking up fights and managing drunk kids. However, when he rescues Terry, the club owner and local gang leader, from a hitman, Danny is suddenly plunged deep into an underworld he had tried so hard to avoid. Danny is promoted to manager, but only on the condition that he turns a blind eye to the shady goings on both on and off the dance-floor. Danny and his girlfriend Jessica dream of setting up their own club, but Danny is forced to make the ultimate sacrifice when he has to choose between loyalty, morality and his own dreams of escape",
+    "popularity": 2.89,
+    "poster_path": "/gR2UN3Bu8c4RYnp7RwCC6nKwMnh.jpg",
+    "production_companies": [
+        {
+            "id": 25318,
+            "logo_path": null,
+            "name": "Fourth Culture Films",
+            "origin_country": ""
+        },
+        {
+            "id": 25316,
+            "logo_path": null,
+            "name": "Dangerous Productions",
+            "origin_country": ""
+        },
+        {
+            "id": 25315,
+            "logo_path": null,
+            "name": "FineLight Films",
+            "origin_country": ""
+        },
+        {
+            "id": 21532,
+            "logo_path": null,
+            "name": "Motion Picture House",
+            "origin_country": ""
+        }
+    ],
+    "production_countries": [
+        {
+            "iso_3166_1": "GB",
+            "name": "United Kingdom"
+        }
+    ],
+    "release_date": "2010-06-02",
+    "revenue": 0,
+    "runtime": 96,
+    "spoken_languages": [
+        {
+            "iso_639_1": "en",
+            "name": "English"
+        }
+    ],
+    "status": "Released",
+    "tagline": "Every friendship has its price",
+    "title": "Baseline",
+    "video": false,
+    "vote_average": 5.3,
+    "vote_count": 6
+}
+       ''',
+            200,
+            headers: {
+              HttpHeaders.contentTypeHeader: 'application/json; charset=utf-8'
+            });
+      });
+
+      MovieDetail moviesDetail = await repository.getMovieDetails(41342);
+
+      //expect(movies.length, 1);
+      expect(moviesDetail.adult, false);
+      expect(moviesDetail.homepage, 'http://www.baselinethemovie.com');
+      expect(moviesDetail.backdropPath, "/zNYbiNjYaeK8EDGFjXUJL7E54Bv.jpg");
+      expect(moviesDetail.id, 41342);
+    });
   });
 }
